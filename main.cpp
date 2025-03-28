44788708a994f0ad28cc8f0e5cefb4d6340ec6c6
Sam <smmathis97@gmail.com>
2025-03-28 11:08:45 -0600

    hello world ten times

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
+// Description: A simple C++ program that prints "Hello, World!" to the console.
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main()
+{
+    std::cout << "Sam Mathis" << std::endl;
+    std::cout << "Sam Mathis" << std::endl;
+    std::cout << "Sam Mathis" << std::endl;
+    std::cout << "Sam Mathis" << std::endl;
+    std::cout << "Sam Mathis" << std::endl;
+    std::cout << "Sam Mathis" << std::endl;
+    std::cout << "Sam Mathis" << std::endl;
+    std::cout << "Sam Mathis" << std::endl;
+    std::cout << "Sam Mathis" << std::endl;
+    std::cout << "Sam Mathis" << std::endl;
     return 0;
 }
